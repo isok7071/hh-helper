bdf30575fa15855ae63a62a50fa1f68fbe25e4e2
Igor Sokolov <isok7071@gmail.com>
2024-12-05 14:24:01 +0300

    Добавляет начальную конфигурацию, пакет для авторизации

---
+++ app/Services/HeadHunter/HeadHunterApiClient.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\HeadHunter;
+
+use AlexMasterov\OAuth2\Client\Provider\HeadHunter;
+
+class HeadHunterApiClient
+{
+    public function exec(): void
+    {
+    }
+
+    private function auth(): void
+    {
+    }
+}
