f3c27aeb403da9b271c2f3896ce43e8464b3bd2a
Igor Sokolov <isok7071@gmail.com>
2024-12-06 10:01:43 +0300

    +

--- app/Services/HeadHunter/HeadHunterApiClient.php
+++ app/Services/HeadHunter/HeadHunterApiClient.php
@@ -3,14 +3,21 @@
 namespace App\Services\HeadHunter;
 
 use AlexMasterov\OAuth2\Client\Provider\HeadHunter;
+use Illuminate\Container\Attributes\Config;
 
 class HeadHunterApiClient
 {
+    public function __construct(private HeadHunter $client)
+    {
+    }
+
     public function exec(): void
     {
     }
 
-    private function auth(): void
+    //TODO токен хранить как кэш можно
+
+    private function getToken()
     {
     }
 }
---
+++ app/Services/HeadHunter/TokenExceptions.php
@@ -0,0 +1,6 @@
+<?php
+
+
+class TokenException extends Exception
+{
+}
