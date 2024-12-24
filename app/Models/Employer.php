de29c21fd094555f1933aa0462ea189113a2c370
Igor Sokolov <isok7071@gmail.com>
2024-12-24 18:04:47 +0300

    Типизация phpdoc

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $external_id
+ * @property string $name
+ * @property string $url
+ * @property int $trusted
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\EmployerFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereExternalId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereTrusted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereUrl($value)
+ * @mixin \Eloquent
+ */
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
--- app/Models/Vacancy.php
+++ app/Models/Vacancy.php
@@ -5,6 +5,43 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $external_id
+ * @property string $name
+ * @property string $url
+ * @property int $employer_id
+ * @property string|null $salaryFrom
+ * @property string|null $salaryTo
+ * @property int|null $gross
+ * @property string|null $currency
+ * @property string $adress_raw
+ * @property int $experience_id
+ * @property string $published_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\VacancyFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereAdressRaw($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereCurrency($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereEmployerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereExperienceId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereExternalId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereGross($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy wherePublishedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereSalaryFrom($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereSalaryTo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Vacancy whereUrl($value)
+ * @mixin \Eloquent
+ */
 class Vacancy extends Model
 {
     /** @use HasFactory<\Database\Factories\VacancyFactory> */
